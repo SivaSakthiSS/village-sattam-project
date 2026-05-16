f5a766a9956e2059b2153b825d2ea21118cd6d3c
Sivasakthi S <siva13042006@gmail.com>
2026-05-16 09:32:43 +0530

    Refactor DATABASE_URL to use environment variable
    Remove hardcoded DATABASE_URL and use environment variable.

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 import os
 from urllib.parse import urlparse
 
-DATABASE_URL = os.getenv('DATABASE_URL', "mysql://root:VUIbHwcSoPDoUXMqKOAsrZxFUblnHAgP@hopper.proxy.rlwy.net:55188/railway")
+DATABASE_URL = os.getenv('DATABASE_URL')
 
     url = urlparse(DATABASE_URL)
 
