4dc2b07cd6b0d85848d9eee675a3d2115c8a0e1b
Sivasakthi S <siva13042006@gmail.com>
2026-05-16 09:36:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,9 @@
 import os
 from urllib.parse import urlparse
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'mysql://avnadmin:AVNS_HxRpLfGNck3U3pzwLpo@siva-siva.c.aivencloud.com:15802/defaultdb?ssl-mode=REQUIRED')
+DATABASE_URL = os.getenv('DATABASE_URL', "mysql://avnadmin:AVNS_HxRpLfGNck3U3pzwLpo@siva-siva.c.aivencloud.com:15802/defaultdb?ssl-mode=REQUIRED")
 
+if DATABASE_URL:
     url = urlparse(DATABASE_URL)
 
     class Config:
